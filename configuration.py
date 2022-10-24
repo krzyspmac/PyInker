0232e1743b08948aafd705f9f4a81fe5b504cdc0
Krzysztof Pawłowski <krzyspmac@gmail.com>
2022-10-24 20:56:37 +0200

    Configuration class added

---
+++ configuration.py
@@ -0,0 +1,36 @@
+import yaml
+from general import FontList
+from general import ColorList
+
+class Configuration:
+    dict = {}
+
+    def __init__(self, filename):
+
+        with open(filename, "r") as stream:
+            try:
+                self._dict = yaml.safe_load(stream)
+                print(self.raw)
+                FontList.from_config(self.fonts)
+                ColorList.from_config(self.colors)
+            except yaml.YAMLError as exc:
+                print(exc)
+        pass
+
+    @property
+    def raw(self):
+        return self._dict
+
+    @property
+    def screen(self):
+        return self._dict["screen"]
+
+    @property
+    def fonts(self):
+        return self._dict["fonts"]
+
+    @property
+    def colors(self):
+        return self._dict["colors"]
+
+    pass # Configuration
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from general import FontList
 from general import ColorList
 from general import Rect
 from widgets import TextWidget
+from configuration import Configuration
+
 import yaml
 from yaml import load, dump
 from yaml import Loader, Dumper
@@ -14,9 +16,18 @@ from sample_view import SampleView
 import curses
 import os
 import time
+from threading import Thread
 
 # Configuration
 display = {'width': 320, 'height': 200}
+configuration: Configuration
+
+def setup():
+    global configuration
+    global display
+    configuration = Configuration("config.yml")
+    display = {'width': configuration.screen["width"], 'height': configuration.screen["height"]}
+    pass
 
 def draw():
     image = Image.new("RGB", (display['width'], display['height']), color=ColorList.get_color("White").rgb)
@@ -29,35 +40,26 @@ def draw():
     image.show()
     pass
 
-def setup():
-    global display
-    with open("config.yml", "r") as stream:
-        try:
-            cfg = yaml.safe_load(stream)
-            print(cfg["screen"])
-            width = int(cfg["screen"]["width"])
-            print(width)
-
-            screen = cfg["screen"]
-            display = {'width': int(screen["width"]), 'height': int(screen["height"])}
-
-            FontList.from_config(cfg["fonts"])
-            ColorList.from_config(cfg["colors"])
-        except yaml.YAMLError as exc:
-            print(exc)
-    pass
-
 setup()
 draw()
 
+def threaded():
+    time.sleep(1)
+    print('This is from another thread')
+    pass
+
 def main(win):
     """The main event queue"""
+    thread = Thread(target=threaded)
+    thread.start()
+
     win.nodelay(True)
     key=""
     win.clear()                
     win.addstr("Detected key:")
     while 1:         
         time.sleep(1/60)
+        print("Wait\r")
         try:                 
            key = win.getkey()         
            win.clear()                
@@ -68,5 +70,17 @@ def main(win):
         except Exception as e:
            # No input   
            pass         
+
+def main2():
+    thread = Thread(target=threaded)
+    thread.start()
+
+    while 1:
+        print("From main thread\n")
+        time.sleep(0.3)
+        pass
+    pass
+
+# main2()
 
 curses.wrapper(main)
