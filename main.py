3c78c53aa9d7a71e999e9aa931ca30ec4401ae41
x180611 <krzysztof.pawlowski@bankmillennium.pl>
2022-10-24 15:11:47 +0200

    Main loop

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ import yaml
 from yaml import load, dump
 from yaml import Loader, Dumper
 from sample_view import SampleView
+# from pynput.keyboard import Key, Listener
+import curses
+import os
+import time
 
 # Configuration
 display = {'width': 320, 'height': 200}
@@ -25,19 +29,44 @@ def draw():
     image.show()
     pass
 
-with open("config.yml", "r") as stream:
-    try:
-        cfg = yaml.safe_load(stream)
-        print(cfg["screen"])
-        width = int(cfg["screen"]["width"])
-        print(width)
+def setup():
+    global display
+    with open("config.yml", "r") as stream:
+        try:
+            cfg = yaml.safe_load(stream)
+            print(cfg["screen"])
+            width = int(cfg["screen"]["width"])
+            print(width)
 
-        screen = cfg["screen"]
-        display = {'width': int(screen["width"]), 'height': int(screen["height"])}
+            screen = cfg["screen"]
+            display = {'width': int(screen["width"]), 'height': int(screen["height"])}
 
-        FontList.from_config(cfg["fonts"])
-        ColorList.from_config(cfg["colors"])
-    except yaml.YAMLError as exc:
-        print(exc)
+            FontList.from_config(cfg["fonts"])
+            ColorList.from_config(cfg["colors"])
+        except yaml.YAMLError as exc:
+            print(exc)
+    pass
 
+setup()
 draw()
+
+def main(win):
+    """The main event queue"""
+    win.nodelay(True)
+    key=""
+    win.clear()                
+    win.addstr("Detected key:")
+    while 1:         
+        time.sleep(1/60)
+        try:                 
+           key = win.getkey()         
+           win.clear()                
+           win.addstr("Detected key:")
+           win.addstr(str(key)) 
+           if key == os.linesep:
+              break           
+        except Exception as e:
+           # No input   
+           pass         
+
+curses.wrapper(main)
